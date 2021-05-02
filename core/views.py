c13842b72145a59fc1c7a9110e55a5dc44bc1a65
Junaid Siddiqui <maxeinstein.dev@gmail.com>
2021-05-02 20:52:11 +0530

    Website - All Features Functional

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from series.views import WebSeries
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    series = WebSeries.objects.all()
+    return render(request, 'index.html', {'series':series})
